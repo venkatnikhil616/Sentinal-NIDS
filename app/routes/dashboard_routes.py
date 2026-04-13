0b043d63874556025f44909893f9db3cbdf8900c
venkat nikhil vallamsetty <venkatnikhil616@gmail.com>
2026-04-13 18:02:24 +0530

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -1,14 +1,15 @@
+import os
 from flask import Blueprint, render_template
-
 from database.crud import get_recent_logs, get_attack_stats
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 dashboard_bp = Blueprint(
     "dashboard",
     __name__,
-    template_folder="../../dashboard/templates",
-    static_folder="../../dashboard/static",
-    static_url_path="/dashboard/static"   # IMPORTANT FIX
+    template_folder=os.path.join(BASE_DIR, "dashboard/templates"),
+    static_folder=os.path.join(BASE_DIR, "dashboard/static"),
+    static_url_path="/dashboard/static"
 )
 
 
